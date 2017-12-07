8b7ab80c3b82d165a9609d6042a5c363e1cdbcd3
Eduardo Silva Navarrete <e.y.silvanavarrete@europeana.eu>
2017-12-07 14:35:22 +0100

    Update JS scrolling fc to add hash when the user stop scrolling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //Add a class (active) to the element with class anchor when getting to the top of the page.
-// TODO: update url along with the class
+// Also the hash url changes when the user stops scrolling.
 $(window).on('scroll', function () {
   $('.anchor').each(function (x, el) {
     var elementID = $(el).parent().parent().attr('id'),
@@ -7,8 +7,21 @@ $(window).on('scroll', function () {
       elementById = 'a[href="#' + elementID + '"';
 
     if (eTop - $(window).scrollTop() < 10) {
+      var parser = document.createElement('a');
+      parser.href = location.href;
       $('.toc').find('.active').removeClass('active');
       $(elementById).addClass('active');
+        if(elementID !== window.location.hash.substr(1)) {
+          // window.history.pushState("", "", '#' +elementID);
+          // return;
+          clearTimeout( $.data( this, "scrollCheck" ) );
+          $.data( this, "scrollCheck", setTimeout(function() {
+             console.log( 'stopped' );
+            window.history.pushState("", "", '#' +elementID);
+          }, 250) );
+
+        }
+
     }
   });
 });
