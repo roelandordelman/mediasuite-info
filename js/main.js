1eeaf62517ca59be60c7d73383de6f3ceff7e169
Eduardo Silva Navarrete <e.y.silvanavarrete@europeana.eu>
2017-12-07 14:58:05 +0100

    Update scrolling hash change and margins/padding

--- js/main.js
+++ js/main.js
@@ -6,21 +6,19 @@ $(window).on('scroll', function () {
       eTop = $('#' + elementID).offset().top,
       elementById = 'a[href="#' + elementID + '"';
 
-    if (eTop - $(window).scrollTop() < 10) {
+    if (eTop - $(window).scrollTop() < 1) {
       var parser = document.createElement('a');
       parser.href = location.href;
       $('.toc').find('.active').removeClass('active');
       $(elementById).addClass('active');
-        if(elementID !== window.location.hash.substr(1)) {
           // changes the hash only when scrolling stops,
           // sort of, sets a timeout for that. No straight
           // way to get that event.
           clearTimeout( $.data( this, "scrollCheck" ) );
           $.data( this, "scrollCheck", setTimeout(function() {
-            window.history.pushState("", "", '#' +elementID);
-          }, 250) );
+            window.history.pushState("", "", '#' +$(el).parent().parent().attr('id'));
+          }, 400) );
 
-        }
 
     }
   });
